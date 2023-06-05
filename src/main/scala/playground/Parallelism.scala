935f7e0f4aa0cd55950a9420fbe815419939d308
antjim1 <antonio.jimeneznieto@epfl.ch>
2023-06-05 12:35:34 +0200

    Add polymorphism to example

--- src/main/scala/playground/Parallelism.scala
+++ src/main/scala/playground/Parallelism.scala
@@ -1,6 +1,7 @@
 package playground
 
 import cats.effect._
+import cats._
 import cats.Traverse
 import cats.syntax.traverse._
 import cats.syntax.parallel._
@@ -8,20 +9,38 @@ import cheatsheet.util._
 
 import scala.util.Random
 import scala.concurrent.duration._
+import cats.effect.kernel.Outcome.Succeeded
+import cats.effect.kernel.Outcome.Errored
+import cats.effect.kernel.Outcome.Canceled
+
+import cats.syntax.functor._ // map
+import cats.syntax.flatMap._ // flatMap
+
+import cats.effect.IO.asyncForIO // implicit Async
 
 object Parallelism {
 
   object gradesExample {
-    def generateRandomGrades(id: Int, numberOfGrades: Int = 5): IO[List[Int]] =
+    def generateRandomGrades(id: Int, numberOfGrades: Int = 5): IO[(Int, List[Int])] =
       val students: List[Int] = (0 to numberOfGrades).toList
       for {
         _ <- IO(s"Fetching grades for student with id: $id from the database...").debug
         _ <- IO.sleep(1.second)
         res <- students.parTraverse(x => IO(Random.nextInt(10)))
-      } yield res
+      } yield (id, res)
 
-    def fetchGrades(): IO[List[List[Int]]] = {
-      val students = (0 to 10).toList
+    def generateRandomGradesPoly[F[_]](id: Int, numberOfGrades: Int = 5)(
+        using F: Async[F],
+        P: Parallel[F]): F[(Int, List[Int])] =
+      val students: List[Int] = (0 to numberOfGrades).toList
+      for {
+        _ <- F.delay(s"Fetching grades for student with id: $id from the database...").debug
+        _ <- F.sleep(1.second)
+        res <- students.parTraverse(x => F.pure(Random.nextInt(10)))
+      } yield (id, res)
+
+    def fetchGrades(): IO[List[(Int, List[Int])]] = {
+      val students = (0 to 100).toList
       for {
         _ <- IO("Fetching students grades from the database...").debug
         _ <- IO.sleep(200.millis)
@@ -29,13 +48,31 @@ object Parallelism {
       } yield studentsGrades
     }
 
-    def computeAverage(grades: List[Int]): IO[Int] = for {
-      _ <- IO("Computing average").debug
+    def fetchGradesPoly[F[_]]()(
+        using F: Async[F],
+        P: Parallel[F]): F[List[(Int, List[Int])]] = {
+      val students = (0 to 100).toList
+      for {
+        _ <- F.delay("Fetching students grades from the database...").debug
+        _ <- F.sleep(200.millis)
+        studentsGrades <- students.parTraverse(id => generateRandomGradesPoly(id))
+      } yield studentsGrades
+
+    }
+
+    def computeAverage(id: Int, grades: List[Int]): IO[Int] = for {
+      _ <- IO(s"Computing average of student with id: $id").debug
       _ <- IO.sleep(1.second)
       res <- IO(grades.sum / grades.length)
     } yield res
 
-    def finalGrades: IO[List[Int]] = for {
+    def computeAveragePoly[F[_]](id: Int, grades: List[Int])(using F: Async[F]): F[Int] = for {
+      _ <- F.delay(s"Computing average of student with id: $id").debug
+      _ <- F.sleep(1.second)
+      res <- F.pure(grades.sum / grades.length)
+    } yield res
+
+    def finalGrades(): IO[List[Int]] = for {
       _ <- IO("Starting computation...").debug
       studentsGrades <- fetchGrades()
       averages <- studentsGrades.parTraverse(computeAverage)
@@ -43,12 +80,22 @@ object Parallelism {
       _ <- IO(s"The final list is: $averages").debug
     } yield averages
 
+    def finalGradesPoly[F[_]]()(using F: Async[F], P: Parallel[F]): F[List[Int]] = for {
+      _ <- F.delay("Starting computation...").debug
+      studentsGrades <- fetchGradesPoly()
+      averages <- studentsGrades.parTraverse(computeAveragePoly)
+      _ <- F.delay("Computation finished").debug
+      _ <- F.delay(s"The final list is: $averages").debug
+    } yield averages
+
   }
+
+
 
 }
 
 object main extends IOApp.Simple {
   override def run: IO[Unit] = {
-    Parallelism.gradesExample.finalGrades.void
+    Parallelism.gradesExample.finalGradesPoly().void
   }
 }
